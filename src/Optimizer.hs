0bd8b07a01ef60061004d1f5c5f97c97e4ae14f3
mauriciosalichs <mauricio.salichs@gmail.com>
2021-04-16 15:22:24 -0300

    intento de terminar inline

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -52,6 +52,11 @@ commonSubExpr :: Term -> Term
 commonSubExpr t = t -- Hacer
 
 -- Creating In Line Functions:
+
+getFreshName :: StateT Int Name
+getFreshName = do i <- get
+                  modify (+1)
+                  return ("__nv" ++ show i)
                                           
 calcSizeOfFun :: Term -> Int -- The Heuristic
 calcSizeOfFun (Lam _ _ _ t) = 1 + calcSizeOfFun t
@@ -71,21 +76,30 @@ getFuns (x:xs) = case (tdeclType x) of
                                       getFuns xs  
                       _         -> getFuns xs
 
-deleteFun :: Name -> [TDecl Term] -> [TDecl Term]
+deleteFun :: Name -> [TDecl Term] -> [TDecl Term] -- Por ahora no la usamos
 deleteFun _ [] = []
 deleteFun n (x:xs) = if (n == tdeclName x) then xs
                                            else x : (deleteFun n xs)
 
-substLam :: [Term] -> Term -> Term
-substLam r (App i t u) = App i (substLam r t) (substLam r u)
-substLam r (Lam i x ty t) = substLam r t
-substLam r (Fix i f fty x xty t) = Fix i f fty x xty (substLam r t)
-substLam r (IfZ i c t e) = IfZ i (substLam r c) (substLam r t) (substLam r e)
-substLam r (Let i x ty d t) = Let i x ty (substLam r d) (substLam r t)
-substLam r (UnaryOp i o t) = UnaryOp i o (substLam r t)
-substLam r (BinaryOp i o t u) = BinaryOp i o (substLam r t) (substLam r u)
-substLam r (V i (Bound n)) = r !! n
-substLam _ t = t
+substLam :: Term -> Int -> Term -> Term
+substLam r k (App i t u) = App i (substLam r k t) (substLam r k u)
+substLam r k (Lam i x ty t) = substLam r k t
+substLam r k (Fix i f fty x xty t) = Fix i f fty x xty (substLam r k t)
+substLam r k (IfZ i c t e) = IfZ i (substLam r k c) (substLam r k t) (substLam r k e)
+substLam r k (Let i x ty d t) = Let i x ty (substLam r k d) (substLam r k t)
+substLam r k (UnaryOp i o t) = UnaryOp i o (substLam r k t)
+substLam r k (BinaryOp i o t u) = BinaryOp i o (substLam r k t) (substLam r k u)
+substLam r k t@(V i (Bound n)) = if (n == k) then r else t
+substLam _ _ t = t
+
+substLamR :: Int -> [Term] ->  Term -> StateT Int Term
+substLamR k [] t = ?
+substLamR k (x:xs) t = case x of
+                            V _ _ -> substLamR (k+1) xs (substLam x k t)
+                            Const _ _ -> substLamR (k+1) xs (substLam x k t)
+                            t' ->  do fr <- getFreshName
+                                      return $ substLamR (k+1) xs (Let NoPos fr ty x $ substLam (V NoPos (Free fr)) k t)
+
 
 replaceApp :: Term -> Name -> [Term] -> Maybe [Term]
 replaceApp (App i t a) n as = replaceApp t n (a:as)
@@ -96,12 +110,12 @@ replaceApp _ _ _ = Nothing
 replaceFun :: Name -> Term -> Term -> Term
 replaceFun n r x@(App i t a) = case (replaceApp t n [a]) of
                                   Just as -> case r of
-                                                  Lam _ _ _ t' -> substLam (reverse as) t'
+                                                  Lam _ _ _ t' -> runStateT (substLamR 0 (reverse as) t') 0
                                                   Fix _ _ _ _ _ t' -> undefined
                                                   _ -> undefined
                                   Nothing -> App i (replaceFun n r t) (replaceFun n r a)
 replaceFun n r (Lam i x ty t) = Lam i x ty (replaceFun n r t)
-replaceFun n r (Fix i f fty x xty t) =Fix i f fty x xty (replaceFun n r t)
+replaceFun n r (Fix i f fty x xty t) = Fix i f fty x xty (replaceFun n r t)
 replaceFun n r (IfZ i c t e) = IfZ i (replaceFun n r c) (replaceFun n r t) (replaceFun n r e)
 replaceFun n r (Let i x ty d t) = Let i x ty (replaceFun n r d) (replaceFun n r t)
 replaceFun n r (UnaryOp i o t) = UnaryOp i o (replaceFun n r t)
@@ -109,8 +123,8 @@ replaceFun n r (BinaryOp i o t u) = BinaryOp i o (replaceFun n r t) (replaceFun 
 replaceFun _ _ t = t
 
 doInline :: [TDecl Term] -> (Name, Term, Int) -> [TDecl Term]
-doInline ts (n, t, i) = if i < 12 then map (apply2Term (replaceFun n t)) ts
-                                 else ts
+doInline ts (n, t, i) = if i < 12 then (apply2Term (replaceFun n t)) ts
+                                  else ts
                       
 inlineFun :: MonadPCF m => [TDecl Term] -> m [TDecl Term]
 inlineFun ts = do let (_, table) = runWriter (getFuns ts)
